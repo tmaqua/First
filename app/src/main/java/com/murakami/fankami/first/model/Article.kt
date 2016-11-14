1792030b7c14b3fd3798c7f24f1922d8c204538c
murakami <tmaqua007@gmail.com>
2016-11-14 20:37:47 +0900

    Add User, Article Model

---
+++ app/src/main/java/com/murakami/fankami/first/model/Article.kt
@@ -0,0 +1,7 @@
+package com.murakami.fankami.first.model
+
+/**
+ * Created by fankami on 西暦16/11/14.
+ */
+
+data class Article(val id: String, val title: String, val url: String, val user: User)
---
+++ app/src/main/java/com/murakami/fankami/first/model/User.kt
@@ -0,0 +1,7 @@
+package com.murakami.fankami.first.model
+
+/**
+ * Created by fankami on 西暦16/11/14.
+ */
+
+data class User(val id: String, val name: String, val profileImageUrl: String)
